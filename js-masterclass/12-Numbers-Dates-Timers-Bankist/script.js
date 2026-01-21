9ea89a69e864eab3813080273fee2ae4e052e7ba
Shaira <sil21019@byui.edu>
2026-01-21 16:27:09 +0000

    191 internationalizing numbers done

--- js-masterclass/12-Numbers-Dates-Timers-Bankist/script.js
+++ js-masterclass/12-Numbers-Dates-Timers-Bankist/script.js
@@ -100,6 +100,14 @@ const formatMovementDate = function(date, locale) {
   // return `${day}/${month}/${year}`;
 }
 
+const formatCur = function(value, locale, currency){
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+  }).format(value)
+
+}
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -119,6 +127,8 @@ const displayMovements = function (acc, sort = false) {
     const date = new Date(movementDate);
 
     const displayDate = formatMovementDate(date, acc.locale);
+    
+    const formattedMov = formatCur(movement, acc.locale, acc.currency);
 
     const html = `
       <div class="movements__row">
@@ -126,7 +136,7 @@ const displayMovements = function (acc, sort = false) {
       i + 1
     } ${type}</div>
          <div class="movements__date">${displayDate}</div>
-        <div class="movements__value">${movement.toFixed(2)}€</div>
+        <div class="movements__value">${formattedMov}</div>
       </div>
     `;
 
@@ -136,19 +146,20 @@ const displayMovements = function (acc, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
+
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
+  labelSumOut.textContent = formatCur(out, acc.locale, acc.currency);
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -158,7 +169,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
+  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
 };
 
 const createUsernames = function (accs) {
@@ -535,3 +546,18 @@ btnSort.addEventListener('click', function (e) {
 // const days1 = calcDaysPassed(new Date(2037, 4, 4), new Date(2037, 4, 14));
 
 // console.log(days1);
+
+const num = 3884762.23;
+
+const options = {
+  style: "currency", //unit, percent (unit is ignored), currency (add currency key)
+  unit: 'mile-per-hour',
+  currency: 'EUR',
+  // useGrouping: false // no separators
+
+}
+
+console.log('US: ', new Intl.NumberFormat('en-US', options).format(num));
+console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('Syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
+console.log('Browser: ', new Intl.NumberFormat(navigator.language, options).format(num));
