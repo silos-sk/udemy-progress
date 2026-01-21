ef3eae8680b7c052292f88a0acde79349d76ca66
Shaira <sil21019@byui.edu>
2026-01-21 16:43:00 +0000

    192 Timers - setTimeout and setInterval done

--- js-masterclass/12-Numbers-Dates-Timers-Bankist/script.js
+++ js-masterclass/12-Numbers-Dates-Timers-Bankist/script.js
@@ -288,7 +288,7 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
+    setTimeout(function(){// Add movement
     currentAccount.movements.push(amount);
 
     // Add loan date
@@ -296,7 +296,8 @@ btnLoan.addEventListener('click', function (e) {
 
     // Update UI
     updateUI(currentAccount);
-  }
+  }, 2500);
+}
   inputLoanAmount.value = '';
 });
 
@@ -547,17 +548,35 @@ btnSort.addEventListener('click', function (e) {
 
 // console.log(days1);
 
-const num = 3884762.23;
+// 191 :: Internationalizing Numbers
+// const num = 3884762.23;
 
-const options = {
-  style: "currency", //unit, percent (unit is ignored), currency (add currency key)
-  unit: 'mile-per-hour',
-  currency: 'EUR',
-  // useGrouping: false // no separators
+// const options = {
+//   style: "currency", //unit, percent (unit is ignored), currency (add currency key)
+//   unit: 'mile-per-hour',
+//   currency: 'EUR',
+//   // useGrouping: false // no separators
 
-}
+// }
 
-console.log('US: ', new Intl.NumberFormat('en-US', options).format(num));
-console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
-console.log('Syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
-console.log('Browser: ', new Intl.NumberFormat(navigator.language, options).format(num));
+// console.log('US: ', new Intl.NumberFormat('en-US', options).format(num));
+// console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+// console.log('Syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
+// console.log('Browser: ', new Intl.NumberFormat(navigator.language, options).format(num));
+
+// 192 Timers :: setTimeout and setInterval
+
+// setTimeout
+const ingredients = ['olives', '']
+
+const pizzaTimer = setTimeout(( (ing1, ing2)=> console.log(`Here is your pizza with ${ing1} and ${ing2}🍕`)), 3000, ...ingredients);
+console.log('Waiting ...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+//setInterval
+setInterval(function(){
+  const now = new Date();
+  console.log(now);
+}, 1000);
+
