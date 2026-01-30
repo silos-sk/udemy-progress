92b7d90f0e1f31afa7712a8c36d6c00c15e2109c
Shaira <sil21019@byui.edu>
2026-01-30 14:01:10 +0000

    198 Selecting, Creating, Deleting Elements done

--- js-masterclass/13-Advanced DOM and Events/script.js
+++ js-masterclass/13-Advanced DOM and Events/script.js
@@ -32,3 +32,43 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+/// LECTURES
+
+// 198 :: Selecting, Creating, and Deleting Elements
+const header = document.querySelector('.header');
+
+const allSections = document.querySelectorAll('section');
+console.log(allSections);
+
+document.getElementById('section--1'); 
+const allButtons = document.getElementsByTagName('button'); // HTML collection; live nums
+
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+
+// Delete elements
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+  message.remove();
+})
+
+
+
+
