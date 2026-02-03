70de97a51cd2405193037f76d4575e70a53110c6
Shaira <sil21019@byui.edu>
2026-02-03 11:37:13 +0000

    205 DOM Traversing done

--- js-masterclass/13-Advanced DOM and Events/script.js
+++ js-masterclass/13-Advanced DOM and Events/script.js
@@ -224,3 +224,34 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
 //   console.log('NAV', e.target, e.currentTarget);
 // })
 
+// 205 :: DOM Traversing
+
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes); // all direct child nodes
+console.log(h1.children); // direct children
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = "var(--gradient-secondary)"; // opposite of queryselector - finds parents no matter how far up DOM tree
+h1.closest('h1').style.background = "var(--gradient-primary)";
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+// sibling nodes:
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el){
+  if(el !== h1) el.style.transform = 'scale(0.5)';
+});
