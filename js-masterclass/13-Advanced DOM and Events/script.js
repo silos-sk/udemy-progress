86bb48f61c2765721bcbfea12320761e8bea73d4
Shaira <sil21019@byui.edu>
2026-02-03 13:24:25 +0000

    208 Sticky navigation - scroll done

--- js-masterclass/13-Advanced DOM and Events/script.js
+++ js-masterclass/13-Advanced DOM and Events/script.js
@@ -143,6 +143,18 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+// 208 :: Sticky Navigation
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', function(e){
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky') ;
+  else nav.classList.remove('sticky');
+
+})
+
+
+
 /// LECTURES
 
 // 198 :: Selecting, Creating, and Deleting Elements
