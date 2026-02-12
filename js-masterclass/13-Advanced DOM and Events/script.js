bcdac1ae4893b80560d7b0927f0ff2ef63885913
Shaira <sil21019@byui.edu>
2026-02-12 15:57:33 +0000

    210 Revealing elements on scroll done

--- js-masterclass/13-Advanced DOM and Events/script.js
+++ js-masterclass/13-Advanced DOM and Events/script.js
@@ -186,7 +186,30 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+// Reveal Sections
+const allSections = document.querySelectorAll('.section');
 
+const revealSection = function(entries, observer){
+  // const [entry] = entries;
+  console.log(entries);
+  entries.forEach(entry =>{
+    if(!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserver(entry.target);
+  })
+};
+
+const sectionObserver = new IntersectionObserver
+(revealSection, {
+    root: null,
+    threshold: 0.15,
+});
+
+allSections.forEach(function(section){
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
 
 /// LECTURES
 
